ae56c695bf5eec0ad6bc951765224e8442462563
moss <mcculloughshelby37@gmail.com>
2025-03-30 18:29:54 -0400

    second commit.

--- mini-project-2/index.js
+++ mini-project-2/index.js
@@ -1,12 +1,23 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
+import { Router } from 'express';
+import Routes from 'express';
+
+import movies from './routes/movies.js';
+import users from './routes/users.js';
+import posts from './routes/posts.js';
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
 
-// Test Route
+// Routes for movies, users, and posts
+app.use('/movies', movies);
+app.use('/users', users);   
+app.use('/posts', posts);
+
 app.get('/', (req, res) => {
     res.send('Backend is running!');
 });
